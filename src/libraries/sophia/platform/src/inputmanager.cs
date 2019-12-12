73e1607853362bd4a48d07521796806371359b92
Dave De Breuck <dave.de.breuck@howest.be>
2019-12-12 10:45:07 +0100

    Implement default inputmanager

---
+++ src/libraries/sophia/platform/src/inputmanager.cs
@@ -0,0 +1,36 @@
+using Sophia.Core;
+using UnityEngine;
+
+namespace Sophia.Platform
+{
+    public class InputManager : IInputManager
+    {
+        //-------------------------------------------------------------------------------------
+        public float getAxis(string axis)
+        {
+            return Input.GetAxis(axis);            
+        }
+
+        //-------------------------------------------------------------------------------------
+        public bool isDown(Sophia.Core.KeyCode code)
+        {
+            return Input.GetKey((UnityEngine.KeyCode)code);
+        }
+        //-------------------------------------------------------------------------------------
+        public bool isUp(Sophia.Core.KeyCode code)
+        {
+            return !Input.GetKey((UnityEngine.KeyCode)code);
+        }
+
+        //-------------------------------------------------------------------------------------
+        public bool isPressed(Sophia.Core.KeyCode code)
+        {
+            return Input.GetKeyDown((UnityEngine.KeyCode)code);
+        }
+        //-------------------------------------------------------------------------------------
+        public bool isReleased(Sophia.Core.KeyCode code)
+        {
+            return Input.GetKeyUp((UnityEngine.KeyCode)code);
+        }
+    }
+}
