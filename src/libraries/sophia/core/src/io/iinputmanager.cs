8e1ab76fca881a27bc25fbb29b498dd12de973e4
Dave De Breuck <dave.de.breuck@howest.be>
2019-12-12 10:40:52 +0100

    Adjust inputmanager interface we should support an axis aswell Adjust inputmanager interface we should support keycodes

--- src/libraries/sophia/core/src/io/iinputmanager.cs
+++ src/libraries/sophia/core/src/io/iinputmanager.cs
@@ -2,9 +2,11 @@ namespace Sophia.Core
 {
     public interface IInputManager
     {
-        bool isDown();
-        bool isUp();
-        bool isPressed();
-        bool isReleased();
+        bool isDown(KeyCode code);
+        bool isUp(KeyCode code);
+        bool isPressed(KeyCode code);
+        bool isReleased(KeyCode code);
+
+        float getAxis(string axis);
     }
 }
