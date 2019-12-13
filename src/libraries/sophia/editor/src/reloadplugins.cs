4d72afa12e83a5f68dbdf7a10334503a9b3f82a4
Dave De Breuck <dave.de.breuck@howest.be>
2019-12-13 15:21:59 +0100

    Start implementation of reload plugins

---
+++ src/libraries/sophia/editor/src/reloadplugins.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Sophia.Editor
+{
+    public class ReloadPlugins : MonoBehaviour
+    {
+        [MenuItem("Tools/Reload Plugins %r")]
+        static void reload()
+        {
+            Debug.Log("Reload Plugins")
+        }
+    }
+}
