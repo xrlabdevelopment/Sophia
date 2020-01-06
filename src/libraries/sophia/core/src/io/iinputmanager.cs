70fa8e62d49708908b00435c123cbcb6324803c4
Dave De Breuck <dave.de.breuck@howest.be>
2020-01-06 11:12:41 +0100

    Adjust the api to the new desktopinputmanager

--- src/libraries/sophia/core/src/io/iinputmanager.cs
+++ src/libraries/sophia/core/src/io/iinputmanager.cs
@@ -2,11 +2,6 @@ namespace Sophia.Core
 {
     public interface IInputManager
     {
-        bool isDown(KeyCode code);
-        bool isUp(KeyCode code);
-        bool isPressed(KeyCode code);
-        bool isReleased(KeyCode code);
-
         float getAxis(string axis);
     }
 }
