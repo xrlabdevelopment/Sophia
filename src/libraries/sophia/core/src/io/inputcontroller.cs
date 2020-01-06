78320767786316d89394cf9199b12db9ae916176
Dave De Breuck <dave.de.breuck@howest.be>
2020-01-06 11:13:19 +0100

    Simplify the code

--- src/libraries/sophia/core/src/io/inputcontroller.cs
+++ src/libraries/sophia/core/src/io/inputcontroller.cs
@@ -4,65 +4,43 @@ namespace Sophia.Core
 {
     public abstract class InputController
     {
-        private readonly List<InputKeyCommand>  input_key_commands = new List<InputKeyCommand>();
-        private readonly List<InputAxisCommand> input_axis_commands = new List<InputAxisCommand>();
-        private readonly List<InputTouchCommand> input_touch_commands = new List<InputTouchCommand>();
+        private readonly Dictionary<string, List<IInputCommand>> input_command_lists = new Dictionary<string, List<IInputCommand>>();
 
         //--------------------------------------------------------------------------------------
         public abstract void initialize();
         //--------------------------------------------------------------------------------------
         public void shutdown()
         {
-            input_key_commands.Clear();
-            input_axis_commands.Clear();
-            input_touch_commands.Clear();
+            foreach(KeyValuePair<string, List<IInputCommand>> input_command_list in input_command_lists)
+                input_command_list.Value.Clear();
+
+            input_command_lists.Clear();
         }
 
         //--------------------------------------------------------------------------------------
         public void processInputFor(ICommandReceiver receiver)
         {
-            processKeyCommands(input_key_commands.FindAll(command => command.isTriggered()), receiver);
-            processAxisCommands(input_axis_commands.FindAll(command => command.isTriggered()), receiver);
-            processTouchCommands(input_touch_commands.FindAll(command => command.isTriggered()), receiver);
+            foreach (KeyValuePair<string, List<IInputCommand>> input_command_list in input_command_lists)
+                processCommands(input_command_list.Value.FindAll(command => command.isTriggered()), receiver);
         }
 
         //--------------------------------------------------------------------------------------
-        protected void addKeyCommand(InputKeyCommand command)
+        protected void addCommand(string listName, IInputCommand command)
         {
-            input_key_commands.Add(command);
-        }
-        //--------------------------------------------------------------------------------------
-        protected void addAxisCommand(InputAxisCommand command)
-        {
-            input_axis_commands.Add(command);
-        }
-        //--------------------------------------------------------------------------------------
-        protected void addTouchCommand(InputTouchCommand command)
-        {
-            input_touch_commands.Add(command);
+            if(input_command_lists.ContainsKey(listName))
+            {
+                input_command_lists[listName].Add(command);
+            }
+            else
+            {
+                input_command_lists[listName] = new List<IInputCommand> { command };
+            }
         }
 
         //--------------------------------------------------------------------------------------
-        private void processKeyCommands(List<InputKeyCommand> commands, ICommandReceiver receiver)
+        private void processCommands(List<IInputCommand> commands, ICommandReceiver receiver)
         {
-            foreach (InputKeyCommand command in commands)
-            {
-                if (command.execute(receiver) && !command.IsMultifuntional) break;
-            }
-                
-        }
-        //--------------------------------------------------------------------------------------
-        private void processAxisCommands(List<InputAxisCommand> commands, ICommandReceiver receiver)
-        {
-            foreach (InputAxisCommand command in commands)
-            {
-                if (command.execute(receiver) && !command.IsMultifuntional) break;
-            }
-        }
-        //--------------------------------------------------------------------------------------
-        private void processTouchCommands(List<InputTouchCommand> commands, ICommandReceiver receiver)
-        {
-            foreach (InputTouchCommand command in commands)
+            foreach (IInputCommand command in commands)
             {
                 if (command.execute(receiver) && !command.IsMultifuntional) break;
             }
