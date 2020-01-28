f07cd1bbddb767511319280cf96cce3a9d99bb35
DAEResearch <daeresearch@howest.be>
2020-01-28 14:27:28 +0100

    Added a temp settings menu in sofia

---
+++ src/libraries/sophia/platform/src/core/tempsettingsmenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Sophia.Platform
+{
+    public class tempsettingsmenu : Singleton<tempsettingsmenu>
+    {
+        public bool SnapRotationSetting = true;
+        protected override void onAwake()
+        {
+           
+        }
+
+        protected override void onStart()
+        {
+           
+        }
+
+        protected override void onUpdate(float dTime)
+        {
+            
+        }
+
+        protected override void onDestroy()
+        {
+            
+        }
+    }
+}
