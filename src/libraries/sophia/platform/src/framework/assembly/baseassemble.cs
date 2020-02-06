e2136c3cb0bf3dc43a53d205395802cf08ef853e
DAEResearch <daeresearch@howest.be>
2020-02-06 09:41:18 +0100

    base assemble has been modified to get the application manager (to get the assembly manager) + has a fucntion to check if you are involved with the assembly

--- src/libraries/sophia/platform/src/framework/assembly/baseassemble.cs
+++ src/libraries/sophia/platform/src/framework/assembly/baseassemble.cs
@@ -10,15 +10,34 @@ namespace Sophia.Platform
         private AssemblyManager assembly_manager = null;
 
         //--------------------------------------------------------------------------------------
+        private void Awake()
+        {
+            assembly_manager = ApplicationManager.Instance.AssemManager;
+        }
+
         private void Start()
         {
-            assembly_manager = AssemblyManager.Instance;
             if (assembly_manager != null)
                 assembly_manager.OnAssemble += onAssemble;
             else
                 Debug.Log(string.Format("The assemblePiece with name {0} doesn't have an assembly manager script", name));
         }
 
+        //--------------------------------------------------------------------------------------
+        protected bool isMyObject(AssemblyPiece assembly2)
+        {
+            var assembly_script = this.GetComponent<AssemblyPiece>();
+            if (assembly_script == null)
+                assembly_script = this.GetComponentInChildren<AssemblyPiece>();
+
+            if (assembly_script != null)
+            {
+                if (assembly2 == assembly_script)
+                    return true;
+            }
+
+            return false;
+        }
         //--------------------------------------------------------------------------------------
         protected abstract void onAssemble(ConnectionPoint connectionPoint1, AssemblyPiece assembly1, ConnectionPoint connectionPoint2, AssemblyPiece assembly2);
     }
