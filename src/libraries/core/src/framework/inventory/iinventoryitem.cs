fa7e0aa552307d12fffdfd75110fc525d2755715
Dyronix <Dyronix@users.noreply.github.com>
2020-02-06 13:11:59 +0100

    Add some more comments

--- src/libraries/core/src/framework/inventory/iinventoryitem.cs
+++ src/libraries/core/src/framework/inventory/iinventoryitem.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Sophia.Core
 {
+    /// <summary>
+    /// An interface for an inventory item
+    /// </summary>
     public interface IInventoryItem
     {
         Guid ID { get; }
--- src/libraries/core/src/framework/inventory/inventorycontroller.cs
+++ src/libraries/core/src/framework/inventory/inventorycontroller.cs
@@ -4,11 +4,22 @@ using System.Linq;
 
 namespace Sophia.Core
 {
+    /// <summary>
+    /// An inventory system
+    /// </summary>
     public class InventoryController
     {
         //--------------------------------------------------------------------------------------
         // Delegate
+        /// <summary>
+        /// A delegate created to fire when an item was added
+        /// </summary>
+        /// <param name="item">The added item</param>
         public delegate void Added(IInventoryItem item);
+        /// <summary>
+        /// A delegate created to fire when an item was removed
+        /// </summary>
+        /// <param name="item">The removed item</param>
         public delegate void Removed(IInventoryItem item);
 
         public Added OnItemAdded;
@@ -19,14 +30,23 @@ namespace Sophia.Core
         private readonly List<IInventoryItem> inventory_items = null;
 
         //--------------------------------------------------------------------------------------
-        public InventoryController(IInventoryItem[] items)
+        /// <summary>
+        /// Constructor for the inventory controller
+        /// </summary>
+        /// <param name="items">Initial items present in the inventory</param>
+        public InventoryController(IInventoryItem[] initialItems)
         {
-            inventory_items = items != null
-                ? new List<IInventoryItem>(items)
+            inventory_items = initialItems != null
+                ? new List<IInventoryItem>(initialItems)
                 : new List<IInventoryItem>();
         }
 
         //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Add a new item to the inventory
+        /// </summary>
+        /// <param name="item">The item to be added</param>
+        /// <returns>Returns true when it was added, false if not</returns>
         public bool add(IInventoryItem item)
         {
             if (inventory_items.Find(i => i.ID == item.ID) == null)
@@ -38,14 +58,24 @@ namespace Sophia.Core
             return true;
         }
         //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Remove an item from the inventory
+        /// </summary>
+        /// <param name="item">Item to be removed</param>
+        /// <returns>Returns true when removed, false when not removed</returns>
         public bool remove(IInventoryItem item)
         {
             return inventory_items.Remove(item);
         }
         //--------------------------------------------------------------------------------------
-        public bool remove(Guid ID)
+        /// <summary>
+        /// Remove an item from the inventory
+        /// </summary>
+        /// <param name="identifier">Identifier of the item to be removed</param>
+        /// <returns>Return true when removed, false when not removed</returns>
+        public bool remove(Guid identifier)
         {
-            int index = inventory_items.FindIndex(i => i.ID == ID);
+            int index = inventory_items.FindIndex(i => i.ID == identifier);
             if (index == -1)
                 return false;
 
@@ -57,18 +87,34 @@ namespace Sophia.Core
         }
 
         //--------------------------------------------------------------------------------------
-        public IInventoryItem get(Guid ID)
+        /// <summary>
+        /// Retrieve an item from the inventory
+        /// </summary>
+        /// <param name="identifier">Identifier of the item to be retrieved</param>
+        /// <returns>The requested item when found, null otherwise</returns>
+        public IInventoryItem get(Guid identifier)
         {
-            return inventory_items.Find(i => i.ID == ID);
+            return inventory_items.Find(i => i.ID == identifier);
         }
         //--------------------------------------------------------------------------------------
-        public T get<T>(Guid ID)
+        /// <summary>
+        /// Retrieve an item from the inventory
+        /// </summary>
+        /// <typeparam name="T">Type of the item to be retrieved</typeparam>
+        /// <param name="identifier">Identifier of the item to be retrieved</param>
+        /// <returns>The requested item, null otherwise</returns>
+        public T get<T>(Guid identifier)
             where T : class, IInventoryItem
         {
-            return inventory_items.Find(i => i.ID == ID) as T;
+            return inventory_items.Find(i => i.ID == identifier) as T;
         }
 
         //--------------------------------------------------------------------------------------
+        /// <summary>
+        /// Retrieve items of a certain type
+        /// </summary>
+        /// <typeparam name="T">The type of the items to be retrieved</typeparam>
+        /// <returns>A list of all items of the specific type</returns>
         public List<IInventoryItem> getItemsOfType<T>()
             where T : class, IInventoryItem
         {
