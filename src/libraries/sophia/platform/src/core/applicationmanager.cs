f6183fff691392da8f2e0a87c0e7cb3291e8813d
DAEResearch <daeresearch@howest.be>
2020-02-06 09:41:42 +0100

    applicationmanager now stores assemblymanager + connection manager

--- src/libraries/sophia/platform/src/core/applicationmanager.cs
+++ src/libraries/sophia/platform/src/core/applicationmanager.cs
@@ -21,9 +21,10 @@ namespace Sophia.Platform
         // Properties
         public abstract ManagerPlatform Platform { get; }
         public abstract ManagerPurpose Purpose { get; }
-
         public abstract EventQueue EventQueue { get; }
         public abstract EventDispatch EventDispatch { get; }
+        public abstract AssemblyManager AssemManager { get; }
+        public abstract ConnectionPointManager ConnectionPointManager { get; }
 
         //--------------------------------------------------------------------------------------
         public abstract void subscribeCommandReceiver(ICommandReceiver receiver);
