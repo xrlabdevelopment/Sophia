0fa1be9fc60e12c892a801e0408e2d7b264bdd44
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-05-28 09:21:08 +0200

    Add an algorithm namespace

--- src/libraries/core/src/core/algorithms/clamp.cs
+++ src/libraries/core/src/core/algorithms/clamp.cs
@@ -1,39 +1,42 @@
-namespace Sophia.Core
+namespace Sophia
 {
-    public static partial class Algorithms
+    namespace Algo
     {
-        /// <summary>
-        /// Clamp a value between a min an a max value
-        /// </summary>
-        /// <param name="value">The value to be clamped</param>
-        /// <param name="min">Minimum value</param>
-        /// <param name="max">Maximum value</param>
-        /// <returns>The clamped value</returns>
-        public static int clamp(int value, int min, int max)
+        public static partial class Algorithms
         {
-            return value < min ? min : value > max ? max : value;
-        }
-        /// <summary>
-        /// Clamp a value between a min an a max value
-        /// </summary>
-        /// <param name="value">The value to be clamped</param>
-        /// <param name="min">Minimum value</param>
-        /// <param name="max">Maximum value</param>
-        /// <returns>The clamped value</returns>
-        public static float clamp(float value, float min, float max)
-        {
-            return value < min ? min : value > max ? max : value;
-        }
-        /// <summary>
-        /// Clamp a value between a min an a max value
-        /// </summary>
-        /// <param name="value">The value to be clamped</param>
-        /// <param name="min">Minimum value</param>
-        /// <param name="max">Maximum value</param>
-        /// <returns>The clamped value</returns>
-        public static double clamp(double value, double min, double max)
-        {
-            return value < min ? min : value > max ? max : value;
+            /// <summary>
+            /// Clamp a value between a min an a max value
+            /// </summary>
+            /// <param name="value">The value to be clamped</param>
+            /// <param name="min">Minimum value</param>
+            /// <param name="max">Maximum value</param>
+            /// <returns>The clamped value</returns>
+            public static int clamp(int value, int min, int max)
+            {
+                return value < min ? min : value > max ? max : value;
+            }
+            /// <summary>
+            /// Clamp a value between a min an a max value
+            /// </summary>
+            /// <param name="value">The value to be clamped</param>
+            /// <param name="min">Minimum value</param>
+            /// <param name="max">Maximum value</param>
+            /// <returns>The clamped value</returns>
+            public static float clamp(float value, float min, float max)
+            {
+                return value < min ? min : value > max ? max : value;
+            }
+            /// <summary>
+            /// Clamp a value between a min an a max value
+            /// </summary>
+            /// <param name="value">The value to be clamped</param>
+            /// <param name="min">Minimum value</param>
+            /// <param name="max">Maximum value</param>
+            /// <returns>The clamped value</returns>
+            public static double clamp(double value, double min, double max)
+            {
+                return value < min ? min : value > max ? max : value;
+            }
         }
     }
 }
