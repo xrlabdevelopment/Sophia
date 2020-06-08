47b5e7ca9a07da2bc63457e74066229becba71ce
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-06-08 12:13:21 +0200

    No need to call phython?

--- installProject.bat
+++ installProject.bat
@@ -1,4 +1,4 @@
 IF NOT EXIST C:\ProgramData\Miniconda3\ GOTO NOCONDA
 call C:\ProgramData\Miniconda3\Scripts\activate
 :NOCONDA
-python "%~dp0cmake\build\installProject.py" "%*"
+"%~dp0cmake\build\installProject.py" "%*"
