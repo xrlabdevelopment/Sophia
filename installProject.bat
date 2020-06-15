119cf0eee5029fc731832e5564544e4ce9e4f284
Dyronix <Dyronix@users.noreply.github.com>
2020-06-15 10:42:01 +0200

    Revert "No need to call phython?"
    This reverts commit 47b5e7ca9a07da2bc63457e74066229becba71ce.

--- installProject.bat
+++ installProject.bat
@@ -1,4 +1,4 @@
 IF NOT EXIST C:\ProgramData\Miniconda3\ GOTO NOCONDA
 call C:\ProgramData\Miniconda3\Scripts\activate
 :NOCONDA
-"%~dp0cmake\build\installProject.py" "%*"
+python "%~dp0cmake\build\installProject.py" "%*"
