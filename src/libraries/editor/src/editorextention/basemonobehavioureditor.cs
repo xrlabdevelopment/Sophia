930d820b8c8fc258b27303ac9dd94e24adb814f5
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-06-17 15:32:05 +0200

    Editor extention

---
+++ src/libraries/editor/src/editorextention/basemonobehavioureditor.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+using Sophia;
+
+namespace Sophia.Editor
+{
+	/// <summary>
+	/// This universal editor makes it possible to add buttons that will execute static methods
+	/// to the inspector by adding the InspectorButton attribute to the method.
+	/// </summary>
+	/// <remarks>You can also add this behaviour to your own editor by extending from BaseEditor, and 
+	/// calling DrawInspectorButtons. </remarks>
+	[CustomEditor(typeof(BaseMonoBehaviour), true)]
+	[CanEditMultipleObjects]
+	public class BaseMonoBehaviourEditor : BaseEditor<BaseMonoBehaviour>
+	{
+        //--------------------------------------------------------------------------------------
+        // Constants
+        private const int column_count = 3;
+
+        #region Unity Messages
+
+        //--------------------------------------------------------------------------------------
+        public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+			DrawInspectorButtons(column_count);
+		}
+
+        #endregion
+    }
+}
