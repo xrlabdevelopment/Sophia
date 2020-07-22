552b271960201330990c8221559a4ee5f5f51264
r_dh <remy.dheygere@howest.be>
2020-07-22 17:19:16 +0200

    build: Remove CMake

--- backup/cmake/build/_generateVC2019_x64_no_fetch.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-del /f /s /q ..\VC2019_x64\CMakeFiles
-del /f /s /q ..\VC2019_x64\CMakeCache.txt
-IF NOT EXIST C:\ProgramData\Miniconda3\ GOTO NOCONDA
-call C:\ProgramData\Miniconda3\Scripts\activate
-python generateProject.py "VC2019_x64"  "Visual Studio 16" -A x64 -DSOPHIA_FETCH_SUBMODULE=0
-:NOCONDA
-"%~dp0generateProject.py" "VC2019_x64"  "Visual Studio 16" -A x64 -DSOPHIA_FETCH_SUBMODULE=0 "%*"
