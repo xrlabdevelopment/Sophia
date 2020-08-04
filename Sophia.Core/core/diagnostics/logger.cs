10c5bd7a56e5eaddf4dbbfe53f62b11c840bdab5
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-08-04 15:15:00 +0200

    feat(core): added the functionality to print the reported message to the .Net Diagnostic listeners

--- Sophia.Core/core/diagnostics/logger.cs
+++ Sophia.Core/core/diagnostics/logger.cs
@@ -12,6 +12,12 @@ namespace Sophia
             public Action<string> log_function;
             public Action<string> warn_function;
             public Action<string> error_function;
+
+            /// <summary>
+            /// Writes the value of the object's System.Object.ToString method to the trace listeners
+            /// in the System.Diagnostics.Debug.Listeners collection.
+            /// </summary>
+            public bool print_to_diagnostic_listeners;
         }
 
         /// <summary>
@@ -26,6 +32,8 @@ namespace Sophia
             private readonly Action<string> warn_function;
             private readonly Action<string> error_function;
 
+            private readonly bool print_to_diagnostic_listeners;
+
             //-------------------------------------------------------------------------------------
             /// <summary>
             /// Constructor of this logger class
@@ -36,6 +44,8 @@ namespace Sophia
                 log_function = info.log_function;
                 warn_function = info.warn_function;
                 error_function = info.error_function;
+
+                print_to_diagnostic_listeners = info.print_to_diagnostic_listeners;
             }
 
             //-------------------------------------------------------------------------------------
@@ -47,6 +57,8 @@ namespace Sophia
             {
                 System.Diagnostics.Debug.Assert(log_function != null, "Log function was not specified");
                 log_function(message);
+                if (print_to_diagnostic_listeners)
+                    System.Diagnostics.Debug.WriteLine("LOG: " + message);
             }
             //-------------------------------------------------------------------------------------
             /// <summary>
@@ -57,6 +69,8 @@ namespace Sophia
             {
                 System.Diagnostics.Debug.Assert(log_function != null, "Log function was not specified");
                 warn_function(message);
+                if (print_to_diagnostic_listeners)
+                    System.Diagnostics.Debug.WriteLine("WARN: " + message);
             }
             //-------------------------------------------------------------------------------------
             /// <summary>
@@ -67,6 +81,8 @@ namespace Sophia
             {
                 System.Diagnostics.Debug.Assert(log_function != null, "Log function was not specified");
                 error_function(message);
+                if (print_to_diagnostic_listeners)
+                    System.Diagnostics.Debug.WriteLine("ERROR: " + message);
             }
         }
     }
