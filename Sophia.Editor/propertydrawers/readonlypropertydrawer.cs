915d2a0bade4a515ea27f3963d49ac619c06554b
Dave De Breuck <dave.de.breuck@howest.be>
2020-08-09 15:47:32 +0200

    fix(editor): Readonly attribute wasn't working properly

--- Sophia.Editor/propertydrawers/readonlypropertydrawer.cs
+++ Sophia.Editor/propertydrawers/readonlypropertydrawer.cs
@@ -1,32 +1,49 @@
-using System.ComponentModel;
+using Sophia.Platform.Attributes;
 using UnityEditor;
 using UnityEngine;
 
 namespace Sophia.Editor.PropertyDrawers
 {
-    /// <summary>
-    /// A property drawer that can be used for read-only fields in the inspector. 
-    /// </summary>
-    /// <seealso cref="UnityEditor.PropertyDrawer" />
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-	public class ReadOnlyPropertyDrawer : PropertyDrawer
-	{
-        #region Unity Messages
-
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
         //-------------------------------------------------------------------------------------
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-		{
-			return EditorGUI.GetPropertyHeight(property, label, true);
-		}
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
 
         //-------------------------------------------------------------------------------------
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-		{
-			EditorGUI.BeginDisabledGroup(true);
-			EditorGUI.PropertyField(position, property, label, true);
-			EditorGUI.EndDisabledGroup();
-		}
+        {
+            using (var scope = new EditorGUI.DisabledGroupScope(true))
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
+        }
+    }
 
-        #endregion
+    [CustomPropertyDrawer(typeof(BeginReadOnlyGroupAttribute))]
+    public class BeginReadOnlyGroupDrawer : DecoratorDrawer
+    {
+        //-------------------------------------------------------------------------------------
+        public override float GetHeight() { return 0; }
+        //-------------------------------------------------------------------------------------
+        public override void OnGUI(Rect position)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(EndReadOnlyGroupAttribute))]
+    public class EndReadOnlyGroupDrawer : DecoratorDrawer
+    {
+        //-------------------------------------------------------------------------------------
+        public override float GetHeight() { return 0; }
+        //-------------------------------------------------------------------------------------
+        public override void OnGUI(Rect position)
+        {
+            EditorGUI.EndDisabledGroup();
+        }
     }
 }
