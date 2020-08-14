134a82f3333441da70b27972603dd6de83b2ea36
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-08-14 13:32:20 +0200

    tests(core): as for now we will postpone the filesystemwatcher tests

---
+++ Sophia.Tests/Sophia.Core/core/filesystem/filesystemwatcher.cs
@@ -0,0 +1,20 @@
+using Sophia.Core.Algorithms;
+
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sophia.Tests.Core
+{
+    [TestFixture]
+    public class testsuit_fileSystemWatcher
+    {
+        //--------------------------------------------------------------------------------------
+        [TestCase]
+        public void test_fileSystemWatcher()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
