8efbbb4a1b0ae8749c1230b1a28a2aaa0e2459c9
r_dh <remy.dheygere@howest.be>
2020-08-25 12:59:11 +0200

    feat: Create separate installer package for initial installation

--- BuildUnityPackages.ps1
+++ BuildUnityPackages.ps1
@@ -1,6 +1,7 @@
 $libraryFiles = @("Sophia.Core.dll", "Sophia.Platform.dll")
+$installerFiles = @("SophiaEditor.cs")
 
-$editorFiles = Get-ChildItem -Path "./Sophia.Editor/*.cs" -Recurse -Exclude "*AssemblyInfo.cs"
+$editorFiles = Get-ChildItem -Path "./Sophia.Editor/*.cs" -Recurse -Exclude ("*AssemblyInfo.cs", "SophiaEditor.cs")
 $editorFiles = $editorFiles.FullName.Replace("\", "/")
 
 $myPath = (Get-Item -Path ".\").FullName;
@@ -11,14 +12,16 @@ $editorBasePath = -join ($myPath, "/Sophia.Editor/")
 
 $builderPath = -join ($myPath, "/Libraries/UnityPackager/UnityPackager.exe")
 $packageOutPath = -join ($myPath, "/Sophia.unitypackage")
+$installerOutPath = -join ($myPath, "/Sophia-Installer.unitypackage")
 
-# Args for library generation
+# Args for library and installer generation
 $libraryBuildArgs = -join ("null", " ", $packageOutPath, " ")
+$installerBuildArgs = -join ("null", " ", $installerOutPath, " ")
 
 # Add library files to library package
 For ($i=0; $i -lt $libraryFiles.Count; $i++)  
 {
-    $libraryBuildArgs += -join ($libraryBasePath, $libraryFiles.Get($i), " ", "Assets/Plugins/Sophia/", $libraryFiles.Get($i), " ")
+    $libraryBuildArgs += -join ($libraryBasePath, $libraryFiles.Get($i), " ", "Assets/Sophia/Lib/", $libraryFiles.Get($i), " ")
 }
 
 # Add editor files to library package under a different path
@@ -27,7 +30,14 @@ For ($i=0; $i -lt $editorFiles.Count; $i++)
     $libraryBuildArgs += -join ($editorFiles.Get($i), " ", "Assets/Editor/Sophia/", $editorFiles.Get($i).Replace($editorBasePath, ""), " ") 
 }
 
-Write-Host $builderPath
-Write-Host $libraryBuildArgs
+# Add installer file(s) to the installer package
+For ($i=0; $i -lt $installerFiles.Count; $i++)  
+{
+    $installerBuildArgs += -join ($editorBasePath, $installerFiles.Get($i), " ", "Assets/Editor/Sophia/", $installerFiles.Get($i).Replace($editorBasePath, ""), " ") 
+}
+
+# Write-Host $builderPath
+# Write-Host $libraryBuildArgs
 
 Start-Process -FilePath $builderPath -ArgumentList $libraryBuildArgs
+Start-Process -FilePath $builderPath -ArgumentList $installerBuildArgs
